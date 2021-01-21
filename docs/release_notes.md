a1c40eb2215eda104a1a0e801c52ac7816eb4944
Mikhail Kravets <michkravets@gmail.com>
2021-01-21 19:32:55 +0200

    Publish (#3)
    * Update setup.py and release notes version
    
    * * Update docs
    * Remove travis conf file
    * Update MANIFEST.in and requirements.txt
    
    * Update docs

--- docs/release_notes.md
+++ docs/release_notes.md
@@ -1,6 +1,6 @@
 # Release Notes
 
-## v0.1.0
+## 0.1.0
 
 ### Features
 
--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ name = 'drf_dog'
 package = 'drf_dog'
 description = 'Copy-paste utilities for Django REST Framework'
 url = 'https://github.com/coaxsoft/drf_dog'
-author = 'Mike Kravets'
+author = 'Mikhail Kravets'
 author_email = 'mike.k@coaxsoft.com'
-license = 'BSD'
+license = 'ISC'
 
 
 def read(f):
@@ -81,23 +81,28 @@ setup(
     packages=get_packages(package),
     package_data=get_package_data(package),
     install_requires=[],
+    keywords='django rest_framework drf utils',
+    python_requires='>=3.6',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Framework :: Django :: 3.0',
         'Framework :: Django :: 3.1',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: ISC License',
         'Operating System :: OS Independent',
         'Natural Language :: English',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Internet :: WWW/HTTP',
-    ]
+    ],
+    project_urls={
+        'Documentation': 'https://coaxsoft.github.io/drf_dog/',
+        'Source': 'https://github.com/coaxsoft/drf_dog',
+    },
 )
